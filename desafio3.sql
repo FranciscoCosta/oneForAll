9430fa25f3c263cc9a6a242aa50cc58ac3a2319c
Xico <francisco100eg@gmail.com>
2022-12-01 08:57:26 -0300

    Requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT a.usuario_nome AS usuario,
+    COUNT(b.musica_id) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(c.duracao_segundos / 60), 2) AS total_minutos       
+FROM SpotifyClone.usuarios AS a
+INNER JOIN SpotifyClone.historico AS b
+ON b.usuario_id = a.usuario_id
+INNER JOIN SpotifyClone.musicas AS c
+ON c.musica_id = b.musica_id
+GROUP BY a.usuario_id
+ORDER BY a.usuario_nome;
+
