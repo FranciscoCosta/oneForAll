b1591a250c3ac5ba6b968306f99616722db5d33e
Xico <francisco100eg@gmail.com>
2022-12-01 08:36:15 -0300

    Requisito 1 corrigido

--- desafio1.sql
+++ desafio1.sql
@@ -28,14 +28,18 @@ CREATE TABLE albuns(
   album_id INT PRIMARY KEY AUTO_INCREMENT,
   album_nome VARCHAR(100) NOT NULL,
   artista_id INT NOT NULL,
+  ano_lancamento YEAR NOT NULL,
   FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 
 CREATE TABLE musicas(
   musica_id INT PRIMARY KEY AUTO_INCREMENT,
+  artista_id INT NOT NULL,
   musica_nome VARCHAR(100) NOT NULL,
+  duracao_segundos INT NOT NULL,
   album_id INT NOT NULL,
-  FOREIGN KEY (album_id) REFERENCES albuns(album_id)
+  FOREIGN KEY (album_id) REFERENCES albuns(album_id),
+  FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 
 
@@ -51,6 +55,7 @@ CREATE TABLE seguir(
 CREATE TABLE historico(
   usuario_id INT NOT NULL,
   musica_id INT NOT NULL,
+  data_reproducao DATETIME NOT NULL,
   PRIMARY KEY (usuario_id, musica_id),
   FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
   FOREIGN KEY (usuario_id) REFERENCES musicas(musica_id)
@@ -86,30 +91,30 @@ VALUES
 
 
 INSERT INTO albuns
-  (album_nome,artista_id)
+  (album_nome,artista_id,ano_lancamento)
 VALUES
-  ('Renaissance',1),
-  ('Jazz',2),
-  ('Hot Space',2),
-  ('Falso Brilhante',3),
-  ('Vento de Maio',3),
-  ('QVVJFA?',4),
-  ('Somewhere Far Beyond',5),
-  ('I Put A Spell On You',6);
+  ('Renaissance',1,2022),
+  ('Jazz',2,1978),
+  ('Hot Space',2,1982),
+  ('Falso Brilhante',3,1998),
+  ('Vento de Maio',3,2001),
+  ('QVVJFA?',4,2003),
+  ('Somewhere Far Beyond',5,2007),
+  ('I Put A Spell On You',6,2012);
 
   INSERT INTO musicas
-  (musica_nome,album_id)
+  (musica_nome, artista_id,album_id, duracao_segundos)
 VALUES
-  ('BREAK MY SOUL',1),
-  ('VIRGO’S GROOVE',1),
-  ('ALIEN SUPERSTAR',1),
-  ('Don’t Stop Me Now',2),
-  ('Under Pressure',3),
-  ('Como Nossos Pais',4),
-  ('O Medo de Amar é o Medo de Ser Livre',5),
-  ('Samba em Paris',6),
-  ('The Bard’s Song',7),
-  ('Feeling Good',8);
+  ('BREAK MY SOUL',1,1,279),
+  ('VIRGO’S GROOVE',1,1,369),
+  ('ALIEN SUPERSTAR',1,1,116),
+  ('Don’t Stop Me Now',2,2,203),
+  ('Under Pressure',2,3,152),
+  ('Como Nossos Pais',3,4,105),
+  ('O Medo de Amar é o Medo de Ser Livre',3,5,207),
+  ('Samba em Paris',4,6,267),
+  ('The Bard’s Song',5,7,244),
+  ('Feeling Good',6,8,100);
 
 INSERT INTO seguir
   (usuario_id, artista_id)
@@ -130,23 +135,23 @@ VALUES
   (10,2);
 
 INSERT INTO historico
-  (usuario_id, musica_id)
+  (usuario_id, musica_id,data_reproducao)
 VALUES
-  (1, 8),
-  (1, 2),
-  (1, 10),
-  (2, 10),
-  (2, 7),
-  (3, 10),
-  (3, 2),
-  (4, 8),
-  (5, 8),
-  (5, 5),
-  (6, 7),
-  (6, 1),
-  (7, 4),
-  (8, 4),
-  (9, 9),
-  (10, 3);
+	(1,8,'2022-02-28 10:45:55'),
+	(1,2,'2020-05-02 05:30:35'),
+	(1,10,'2020-03-06 11:22:33'),
+	(2,10,'2022-08-05 08:05:17'),
+	(2,7,'2020-01-02 07:40:33'),
+	(3,10,'2020-11-13 16:55:13'),
+	(3,2,'2020-12-05 18:38:30'),
+	(4,8,'2021-08-15 17:10:10'),
+	(5,8,'2022-01-09 01:44:33'),
+	(5,5,'2020-08-06 15:23:43'),
+	(6,7,'2017-01-24 00:31:17'),
+	(6,1,'2017-10-12 12:35:20'),
+	(7,4,'2011-12-15 22:30:49'),
+	(8,4,'2012-03-17 14:56:41'),
+	(9,9,'2022-02-24 21:14:22'),
+	(10,3,'2015-12-13 08:30:22');
 
   
---
+++ desafio2.sql
@@ -0,0 +1,10 @@
+
+SELECT
+	COUNT(DISTINCT musicas.musica_id) AS cancoes,
+    COUNT(DISTINCT artistas.artista_id) AS artistas,
+    COUNT(DISTINCT albuns.album_id) AS albuns
+FROM SpotifyClone.musicas
+INNER JOIN SpotifyClone.artista AS artistas
+ON musicas.artista_id = artistas.artista_id
+INNER JOIN SpotifyClone.albuns AS albuns
+ON musicas.album_id = albuns.album_id;
