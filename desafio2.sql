9b88918bdde5750ace5adb0058632e662c3c1d78
Xico <francisco100eg@gmail.com>
2022-12-01 08:39:41 -0300

    Requisito 2 corrigido

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,9 @@
-
 SELECT
 	COUNT(DISTINCT musicas.musica_id) AS cancoes,
     COUNT(DISTINCT artistas.artista_id) AS artistas,
     COUNT(DISTINCT albuns.album_id) AS albuns
 FROM SpotifyClone.musicas
-INNER JOIN SpotifyClone.artista AS artistas
+INNER JOIN SpotifyClone.artistas AS artistas
 ON musicas.artista_id = artistas.artista_id
 INNER JOIN SpotifyClone.albuns AS albuns
 ON musicas.album_id = albuns.album_id;
